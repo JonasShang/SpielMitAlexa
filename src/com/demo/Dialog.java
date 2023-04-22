3140e0b22ea4260eb7fd6e6e152a38dca26e393d
ZhixiangShang <76771996+JonasShang@users.noreply.github.com>
2023-04-22 22:41:25 +0200

    修复bug

--- src/com/demo/Dialog.java
+++ src/com/demo/Dialog.java
@@ -1,14 +1,25 @@
 package com.demo;
 
+
 import javax.swing.*;
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Zhixiang Shang on 22.04.2023.
  */
 public class Dialog extends JFrame implements ActionListener {
+
+    private static File file = null;
+    //创建audioclip对象
+    private static AudioClip audioClip = null;
+
     public  Dialog(String message){
         this.setVisible(true);
         this.setBounds(150,150,1200,1000);
@@ -25,9 +36,30 @@ public class Dialog extends JFrame implements ActionListener {
         label.setBounds(0,0, 1000,900);
         container.add(label);
         createWindow();
+        play("./sound/winner.mp3");
         //center text
         label.setHorizontalAlignment(SwingConstants.CENTER);
 
+    }
+    public static void play(String fileUrl) {
+        //选择播放文件
+        file = new File(fileUrl);
+
+
+        //将file转换为url
+        try {
+            audioClip = Applet.newAudioClip(file.toURI().toURL());
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //循环播放	播放一次可以使用audioClip.play
+        audioClip.loop();
+//		audioClip.play();
+
+    }
+    public static void stop() {
+        audioClip.stop();
     }
     public void createWindow(){
         ImageIcon arrowIcon = null;
